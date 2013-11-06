3c9e347c4c38066a0fc711f4467d05a3af093995
alcoscist <alcoscist@gmail.com>
2013-11-06 16:38:51 +0300

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parovoz::Application.config.secret_token = '208bb91ae7b48f916be24228bb8a07b65a0deb5fd38c2aa930927d08d144792b1594cfc559222be405fcd2e4257a119e9006b9656c76f587a69d2b89f519ede4'
